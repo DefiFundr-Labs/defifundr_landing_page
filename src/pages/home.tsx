42edcd514f4c5ffd7c118407b089a04f25f4c39c
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-02-25 09:19:31 +0100

    feat:implement cta section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
+import Cta from "../components/home/Cta";
 import Hero from "../components/home/Hero";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <Cta />
     </main>
   );
 };
